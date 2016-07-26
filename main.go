a12da41df2188d63749c03dafe12622e6f19b1a7
Inspectre Gadget <inspectre@inspect.re>
2016-07-26 15:40:20 -0400

    Can now send requests to multiple targets.

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"crypto/tls"
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -15,6 +14,7 @@ import (
 	"sync"
 	"time"
 
+	// Viper is used to parse config files.
 	"github.com/spf13/viper"
 )
 
@@ -52,19 +52,20 @@ var requestsCount = 100
 // Verbose logging (default false)
 var verbose = false
 
+// Usage message
+// TODO: Set this usage message
+var usage string = ``
+
 // Function main is the entrypoint to the application. It sends the work to the appropriate functions, sequentially.
 func main() {
 	// Change output location of logs
 	log.SetOutput(os.Stdout)
 
-	// Set the usage string
-	setUsage()
-
 	// Check the config file
 	err := checkConfig()
 	if err != nil {
 		log.Println(err.Error())
-		flag.Usage()
+		fmt.Println(usage)
 		os.Exit(1)
 	}
 
@@ -104,14 +105,6 @@ func main() {
 
 	// Echo completion
 	log.Println("Complete.")
-}
-
-// Function setUsage overrides the flag package's usage output to something
-// more specific to this package
-func setUsage() {
-	flag.Usage = func() {
-		// TODO: Add in usage output
-	}
 }
 
 // Function checkConfig checks that all necessary configuration fields are given
@@ -203,103 +196,105 @@ func checkConfig() error {
 // Errors are passed back in a channel of errors. If the length is zero, there were no errors.
 func sendRequests() (responses chan *http.Response, errors chan error) {
 	// Initialize the concurrency objects
-	responses = make(chan *http.Response, requestsCount)
-	errors = make(chan error, requestsCount)
-	urlsInProgress.Add(requestsCount)
+	responses = make(chan *http.Response, requestsCount*len(targetURLs))
+	errors = make(chan error, requestsCount*len(targetURLs))
+	urlsInProgress.Add(requestsCount * len(targetURLs))
 
-	// TODO: Send requests to multiple URLs (if present) the same number of times
-	// RESUME
-
-	// VERBOSE
-	if verbose {
-		log.Printf("[VERBOSE] Sending %d %s requests to %s\n", requestsCount, requestMethod, targetURL.String())
-		if body != "" {
-			log.Printf("[VERBOSE] Request body: %s", body)
-		}
-	}
-	for i := 0; i < numRequests; i++ {
-		go func(index int) {
-			// Ensure that the waitgroup element is returned
-			defer urlsInProgress.Done()
-
-			// Convert the request body to an io.Reader interface, to pass to the request.
-			// This must be done in the loop, because any call to client.Do() will
-			// read the body contents on the first time, but not any subsequent requests.
-			requestBody := strings.NewReader(body)
-
-			// Declare HTTP request method and URL
-			req, err := http.NewRequest(requestMethod, targetURL.String(), requestBody)
-			if err != nil {
-				errors <- fmt.Errorf("Error in forming request: %v", err.Error())
-				return
-			}
-
-			// Create the HTTP client
-			// Using Cookie jar
-			// Ignoring TLS errors
-			// Ignoring redirects (more accurate output), depending on user flag
-			// Implementing a connection timeouts, for slow clients & servers (especially important with race conditions on the server)
-			var client http.Client
-			if followRedirects {
-				client = http.Client{
-					Jar: jar,
-					Transport: &http.Transport{
-						TLSClientConfig: &tls.Config{
-							InsecureSkipVerify: true,
-						},
-					},
-					Timeout: 20 * time.Second,
-				}
-			} else {
-				client = http.Client{
-					Jar: jar,
-					Transport: &http.Transport{
-						TLSClientConfig: &tls.Config{
-							InsecureSkipVerify: true,
-						},
-					},
-					CheckRedirect: func(req *http.Request, via []*http.Request) error {
-						// Craft the custom error
-						redirectError := RedirectError{req}
-						return &redirectError
-					},
-					Timeout: 20 * time.Second,
+	// Send requests to multiple URLs (if present) the same number of times
+	for _, target := range targetURLs {
+		go func(t *url.URL) {
+			// VERBOSE
+			if verbose {
+				log.Printf("[VERBOSE] Sending %d %s requests to %s\n", requestsCount, requestMethod, t.String())
+				if body != "" {
+					log.Printf("[VERBOSE] Request body: %s", body)
 				}
 			}
+			for i := 0; i < requestsCount; i++ {
+				go func(index int) {
+					// Ensure that the waitgroup element is returned
+					defer urlsInProgress.Done()
 
-			// Make the request
-			resp, err := client.Do(req)
-			// Check the error type from the request
-			if err != nil {
-				if uErr, ok := err.(*url.Error); ok {
-					if rErr, ok2 := uErr.Err.(*RedirectError); ok2 {
-						// Redirect error
-						// VERBOSE
-						if *flagVerbose {
-							log.Printf("[VERBOSE] %v\n", rErr)
-						}
-						// Add the response to the responses channel, because it is still valid
-						responses <- resp
-					} else {
-						// URL Error, but not a redirect error
-						errors <- fmt.Errorf("Error in request #%v: %v\n", index, err)
+					// Convert the request body to an io.Reader interface, to pass to the request.
+					// This must be done in the loop, because any call to client.Do() will
+					// read the body contents on the first time, but not any subsequent requests.
+					requestBody := strings.NewReader(body)
+
+					// Declare HTTP request method and URL
+					req, err := http.NewRequest(requestMethod, t.String(), requestBody)
+					if err != nil {
+						errors <- fmt.Errorf("Error in forming request: %v", err.Error())
+						return
 					}
-				} else {
-					// Other type of error
-					errors <- fmt.Errorf("Error in request #%v: %v\n", index, err)
-				}
-			} else {
-				// Add the response to the responses channel
-				responses <- resp
+
+					// Create the HTTP client
+					// Using Cookie jar
+					// Ignoring TLS errors
+					// Ignoring redirects (more accurate output), depending on user flag
+					// Implementing a connection timeouts, for slow clients & servers (especially important with race conditions on the server)
+					var client http.Client
+					if followRedirects {
+						client = http.Client{
+							Jar: jar,
+							Transport: &http.Transport{
+								TLSClientConfig: &tls.Config{
+									InsecureSkipVerify: true,
+								},
+							},
+							Timeout: 20 * time.Second,
+						}
+					} else {
+						client = http.Client{
+							Jar: jar,
+							Transport: &http.Transport{
+								TLSClientConfig: &tls.Config{
+									InsecureSkipVerify: true,
+								},
+							},
+							CheckRedirect: func(req *http.Request, via []*http.Request) error {
+								// Craft the custom error
+								redirectError := RedirectError{req}
+								return &redirectError
+							},
+							Timeout: 20 * time.Second,
+						}
+					}
+
+					// Make the request
+					resp, err := client.Do(req)
+					// Check the error type from the request
+					if err != nil {
+						if uErr, ok := err.(*url.Error); ok {
+							if rErr, ok2 := uErr.Err.(*RedirectError); ok2 {
+								// Redirect error
+								// VERBOSE
+								if verbose {
+									log.Printf("[VERBOSE] %v\n", rErr)
+								}
+								// Add the response to the responses channel, because it is still valid
+								responses <- resp
+							} else {
+								// URL Error, but not a redirect error
+								errors <- fmt.Errorf("Error in request #%v: %v\n", index, err)
+							}
+						} else {
+							// Other type of error
+							errors <- fmt.Errorf("Error in request #%v: %v\n", index, err)
+						}
+					} else {
+						// Add the response to the responses channel
+						responses <- resp
+					}
+				}(i)
 			}
-		}(i)
+		}(target)
 	}
 
 	// Wait for the URLs to finish sending
 	urlsInProgress.Wait()
 
 	// VERBOSE
-	if *flagVerbose {
+	if verbose {
 		log.Printf("[VERBOSE] Requests complete.")
 	}
 
@@ -321,7 +316,7 @@ func compareResponses(responses chan *http.Response) (uniqueResponses map[*http.
 	errors = make(chan error, len(responses))
 
 	// VERBOSE
-	if *flagVerbose {
+	if verbose {
 		log.Printf("[VERBOSE] Unique response comparison begin.\n")
 	}
 
@@ -374,7 +369,7 @@ func compareResponses(responses chan *http.Response) (uniqueResponses map[*http.
 	}
 
 	// VERBOSE
-	if *flagVerbose {
+	if verbose {
 		log.Printf("[VERBOSE] Unique response comparision complete.\n")
 	}
 
@@ -427,5 +422,4 @@ func readResponseBody(resp *http.Response) (content []byte, err error) {
 	return
 }
 
-// TODO: Add option to send a second request body or URL at the same time, the same number of times (useful for adding 2 values to a database)
 // TODO: Add option to include multiple session cookie values. Cookies for each request will be semicolon-delimited, and newline characters will delimit cookies for different requests.
